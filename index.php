063ee55bf6769e69fb1688d3bc0a7c6b2fba752f
Paulo.Rocha <paulorochajesus72@gmail.com>
2022-10-28 18:06:58 -0300

    style da pagina listar finalizado com uma tabela,botões para o crud e para a paginação

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include("include/header.php");
 ?>
 
 <div>
-	<link rel="stylesheet" href="styles/global.css">
+	<!-- <link rel="stylesheet" href="styles/global.css"> -->
 	<nav class="navbar navbar-expand-lg bg-light">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="#">Navbar</a>
@@ -54,7 +54,7 @@ include("include/header.php");
 	}
 
 	//Setar a quantidade de registros por pagina 
-	$limite_resultado = 5;
+	$limite_resultado = 3;
 
 	//Calcular o inicio da visualização
 	$inicio = ($limite_resultado * $pagina) - $limite_resultado;
@@ -63,56 +63,100 @@ include("include/header.php");
 	$query_usuarios = "SELECT id, nome, email FROM cadastros LIMIT $inicio, $limite_resultado";
 	$result_usuarios = $conn->prepare($query_usuarios);
 	$result_usuarios->execute();
-
-
-
-	if (($result_usuarios) and ($result_usuarios->rowCount() != 0)) {
-		while ($row_usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)) {
-			//var_dump($row_usuario);
-			extract($row_usuario);
-			echo "ID: $id <br>";
-			echo "Nome: $nome <br>";
-			echo "E-mail: $email <br><br>";
-			echo "<a href='visualizar.php?id=$id'>Visualizar</a><br>";
-			echo "<a href='editar.php?id=$id'>Editar</a><br>";
-			echo "<a href='apagar.php?id=$id'>Apagar</a><br>";
-			echo "<hr>";
-		}
-
-		//Contar a quantidade de registros no BD(Banco de Dados)
-		$query_qnt_registros = "SELECT COUNT(id) AS num_result FROM cadastros";
-		$result_qnt_registros = $conn->prepare($query_qnt_registros);
-		$result_qnt_registros->execute();
-		$row_qnt_registros = $result_qnt_registros->fetch(PDO::FETCH_ASSOC);
-
-		//Quantidade de pagina
-		$qnt_pagina = ceil($row_qnt_registros['num_result'] / $limite_resultado);
-
-		// Maximo de link
-		$maximo_link = 2;
-
-		echo "<a href='index.php?page=1'> Primeira </a>";
-
-		for ($pagina_anterior = $pagina - $maximo_link; $pagina_anterior <= $pagina - 1; $pagina_anterior++) {
-			if ($pagina_anterior >= 1) {
-				echo "<a href='index.php?page=$pagina_anterior'>$pagina_anterior</a> ";
-			}
-		}
-
-		echo "<a href='#'>$pagina</a>";
-
-		for ($proxima_pagina = $pagina + 1; $proxima_pagina <= $pagina + $maximo_link; $proxima_pagina++) {
-			if ($proxima_pagina <= $qnt_pagina) {
-				echo "<a href='index.php?page=$proxima_pagina'>$proxima_pagina</a> ";
-			}
-		}
-
-		echo "<a href='index.php?page=$qnt_pagina'> Última </a>";
-	} else {
-		echo "<p style='color: red;'>Erro: Usuário não cadastrado com sucesso!</p><br>";
-	}
-
 	?>
-	</body>
 
-	</html>
+	<table style="width:100%" class="table table-hover table-striped">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Nome</th>
+				<th>E-mail</th>
+				<th>Ação</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php
+			if (($result_usuarios) and ($result_usuarios->rowCount() != 0)) {
+				while ($row_usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC)) {
+					//var_dump($row_usuario);
+					extract($row_usuario);
+
+			?>
+
+
+					<tr>
+						<td>
+							<?= $id  ?>
+						</td>
+						<td>
+							<div class="d-flex">
+								<img src="https://avatars.dicebear.com/4.5/api/initials/<?= $nome ?>.svg?margin=5&backgroundColorLevel=800" style='width: 30px;height:30px;' class="mr-2">
+								<?= $nome ?>
+							</div>
+						</td>
+						<td><?= $email ?></td>
+						<td>
+							<?php
+							echo "<a class='btn btn-info ml-2' href='visualizar.php?id=$id'> <i class='fas fa-eye mr-2'></i>info</a>";
+							echo "<a class='btn btn-warning ml-2' href='editar.php?id=$id'><i class='fas fa-user-edit mr-2'></i>Edit</a>";
+							echo "<a class='btn btn-danger ml-2' href='apagar.php?id=$id'><i class='fas fa-trash-alt mr-2'></i>Delete</a>";
+							?>
+						</td>
+					</tr>
+				<?php
+				}
+				?>
+		</tbody>
+	</table>
+
+<?php
+
+				//Contar a quantidade de registros no BD(Banco de Dados)
+				$query_qnt_registros = "SELECT COUNT(id) AS num_result FROM cadastros";
+				$result_qnt_registros = $conn->prepare($query_qnt_registros);
+				$result_qnt_registros->execute();
+				$row_qnt_registros = $result_qnt_registros->fetch(PDO::FETCH_ASSOC);
+
+				//Quantidade de pagina
+				$qnt_pagina = ceil($row_qnt_registros['num_result'] / $limite_resultado);
+
+				// Maximo de link
+				$maximo_link = 2;
+
+
+				echo
+				"<nav aria-label='...'>
+					<ul class='pagination'>
+				  	<li class='page-item'>
+							<a class='page-link' href='index.php?page=1' > Primeira </a></li>";
+
+				for ($pagina_anterior = $pagina - $maximo_link; $pagina_anterior <= $pagina - 1; $pagina_anterior++) {
+					if ($pagina_anterior >= 1) {
+						echo "<li class='page-item'><a class='page-link' href='index.php?page=$pagina_anterior'>$pagina_anterior</a></li> ";
+					}
+				}
+
+				echo "<li class='page-item active'><a class='page-link' href='#'>$pagina</a></li>";
+
+				for ($proxima_pagina = $pagina + 1; $proxima_pagina <= $pagina + $maximo_link; $proxima_pagina++) {
+					if ($proxima_pagina <= $qnt_pagina) {
+
+						echo "<a class='page-link' href='index.php?page=$proxima_pagina'>$proxima_pagina</a> ";
+					}
+				}
+
+				echo
+				"<li class='page-item'><a class='page-link' href='index.php?page=$qnt_pagina'> Última </a>
+						</li>
+					</ul>
+				</nav>";
+			} else {
+				echo "<p style='color: red;'>Erro: Usuário não cadastrado com sucesso!</p><br>";
+			}
+
+?>
+
+</body>
+
+</html>
